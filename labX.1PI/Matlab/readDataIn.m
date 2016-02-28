a292babb5c2f0120a289efe0ffb7a7081243f4ad
ranebrown <rane.brown@gmail.com>
2016-02-28 15:42:51 -0700

    platform independent file read

--- labX.1PI/Matlab/readDataIn.m
+++ labX.1PI/Matlab/readDataIn.m
@@ -6,18 +6,19 @@
 
 % velocity data needs scaled by pi/8000 to be in rad/sec
 
+fs = filesep; % / linux mac and \ windows
 
 % machine 1 data
-m1t1=dlmread('data\vref6_KP0.1548_KI0.4611_dist0_stepat5to7.txt');
-m1t2=dlmread('data\vref6_KP0.469_KI3.558_dist0_stepat5to7.txt');
-m1t3=dlmread('data\vref6_KP0.5472_KI3.558_dist0_stepat5to7.txt');
-m1t4=dlmread('data\vref6_KP1.258_KI14.23_dist0_stepat5to7.txt');
-m1t5=dlmread('data\vref6_KP1.329_KI14.2301_dist0_stepat5to7.txt');
-m1t6=dlmread('data\vref6_KP2_KI32.0178_dist0_stepat5to7.txt');
+m1t1=dlmread(['data' fs 'vref6_KP0.1548_KI0.4611_dist0_stepat5to7.txt']);
+m1t2=dlmread(['data' fs 'vref6_KP0.469_KI3.558_dist0_stepat5to7.txt']);
+m1t3=dlmread(['data' fs 'vref6_KP0.5472_KI3.558_dist0_stepat5to7.txt']);
+m1t4=dlmread(['data' fs 'vref6_KP1.258_KI14.23_dist0_stepat5to7.txt']);
+m1t5=dlmread(['data' fs 'vref6_KP1.329_KI14.2301_dist0_stepat5to7.txt']);
+m1t6=dlmread(['data' fs 'vref6_KP2_KI32.0178_dist0_stepat5to7.txt']);
 
 % machine 2 data (same values as machine 1)
-m4t1=dlmread('data\machine4_test1.txt');
-m4t2=dlmread('data\machine4_test2.txt');
-m4t3=dlmread('data\machine4_test3.txt');
-m4t4=dlmread('data\machine4_test4.txt');
-m4t5=dlmread('data\machine4_test5.txt');
+m4t1=dlmread(['data' fs 'machine4_test1.txt']);
+m4t2=dlmread(['data' fs 'machine4_test2.txt']);
+m4t3=dlmread(['data' fs 'machine4_test3.txt']);
+m4t4=dlmread(['data' fs 'machine4_test4.txt']);
+m4t5=dlmread(['data' fs 'machine4_test5.txt']);
