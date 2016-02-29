e7f3d9ec98bccd84af77c6046dc5c3dec9343cca
ranebrown <rane.brown@gmail.com>
2016-02-28 21:36:35 -0700

    calculate step info of experimental data

--- labX.1PI/Matlab/comparePlots.m
+++ labX.1PI/Matlab/comparePlots.m
@@ -1,4 +1,4 @@
-function comparePlots(cl,tds)
+function [S,time,vel] = comparePlots(cl,tds)
 % compares matlab step response and collected step response
 % cl = matlab closed loop TF
 % tds = collected data matrix 1501x4
@@ -10,6 +10,7 @@ y = y+6; % offset to align with actual data
 % velocity to step response start time 5
 time = tds(501:1201,1)-5;
 vel = tds(501:1201,3).*(pi/8000);
+S = stepinfo(vel,time);
 
 % plot data
 plot(t,y,time,vel);
