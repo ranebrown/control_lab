1e30ce86731a011aff9c38ac6ec9e2572af4bce7
ranebrown <rane.brown@gmail.com>
2016-02-29 18:11:36 -0700

    changed to a function that returns struct

--- labX.1PI/Matlab/readDataIn.m
+++ labX.1PI/Matlab/readDataIn.m
@@ -1,3 +1,4 @@
+function [data] = readDataIn()
 % tds collected data
 % col 1 = time sec
 % col 2 = position encoder counts 
@@ -9,16 +10,16 @@
 fs = filesep; % / linux mac and \ windows
 
 % machine 1 data
-m1t1=dlmread(['data' fs 'vref6_KP0.1548_KI0.4611_dist0_stepat5to7.txt']);
-m1t2=dlmread(['data' fs 'vref6_KP0.469_KI3.558_dist0_stepat5to7.txt']);
-m1t3=dlmread(['data' fs 'vref6_KP0.5472_KI3.558_dist0_stepat5to7.txt']);
-m1t4=dlmread(['data' fs 'vref6_KP1.258_KI14.23_dist0_stepat5to7.txt']);
-m1t5=dlmread(['data' fs 'vref6_KP1.329_KI14.2301_dist0_stepat5to7.txt']);
-m1t6=dlmread(['data' fs 'vref6_KP2_KI32.0178_dist0_stepat5to7.txt']);
+data.m1t1=dlmread(['data' fs 'vref6_KP0.1548_KI0.4611_dist0_stepat5to7.txt']);
+data.m1t2=dlmread(['data' fs 'vref6_KP0.469_KI3.558_dist0_stepat5to7.txt']);
+data.m1t3=dlmread(['data' fs 'vref6_KP0.5472_KI3.558_dist0_stepat5to7.txt']);
+data.m1t4=dlmread(['data' fs 'vref6_KP1.258_KI14.23_dist0_stepat5to7.txt']);
+data.m1t5=dlmread(['data' fs 'vref6_KP1.329_KI14.2301_dist0_stepat5to7.txt']);
+data.m1t6=dlmread(['data' fs 'vref6_KP2_KI32.0178_dist0_stepat5to7.txt']);
 
 % machine 2 data (same values as machine 1)
-m4t1=dlmread(['data' fs 'machine4_test1.txt']);
-m4t2=dlmread(['data' fs 'machine4_test2.txt']);
-m4t3=dlmread(['data' fs 'machine4_test3.txt']);
-m4t4=dlmread(['data' fs 'machine4_test4.txt']);
-m4t5=dlmread(['data' fs 'machine4_test5.txt']);
+data.m4t1=dlmread(['data' fs 'machine4_test1.txt']);
+data.m4t2=dlmread(['data' fs 'machine4_test2.txt']);
+data.m4t3=dlmread(['data' fs 'machine4_test3.txt']);
+data.m4t4=dlmread(['data' fs 'machine4_test4.txt']);
+data.m4t5=dlmread(['data' fs 'machine4_test5.txt']);
