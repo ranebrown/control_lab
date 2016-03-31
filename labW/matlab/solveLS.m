ecfb9e5ab1dcacdfeb657d762c0bd42b9cc80a8b
BROWN <brownsr@itll.cu.int>
2016-03-31 14:38:39 -0600

    update

--- labW/matlab/solveLS.m
+++ labW/matlab/solveLS.m
@@ -12,6 +12,13 @@
 % bot = struct of bottom encoder position
 [t, volt, bot, mid, posb, posm, velb, velm, accb, accm] = read();
 
-% calculate parameters for u(t) equation
-[u0, u1a, u1b]  = solveU(wo, volt, t)
+% calculate parameters for u(t) equation at each frequency
+[u0.f1, u1a.f1, u1b.f1]  = solveU(wo.f1, volt.f1, t.f1);
+[u0.f2, u1a.f2, u1b.f2]  = solveU(wo.f2, volt.f2, t.f2);
+[u0.f3, u1a.f3, u1b.f3]  = solveU(wo.f3, volt.f3, t.f3);
+[u0.f4, u1a.f4, u1b.f4]  = solveU(wo.f4, volt.f4, t.f4);
+[u0.f5, u1a.f5, u1b.f5]  = solveU(wo.f5, volt.f5, t.f5);
+
+% calcualte parameters for omega_1(t) equation at each frequency
+
 
